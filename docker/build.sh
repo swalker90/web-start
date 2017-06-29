3284619d00393c7b173c5befc477bef9f9f32e03
Stephen Walker <swalker@redhat.com>
2017-06-29 10:17:23 -0400

    Move dockerfiles to thier own files and update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-cp -rf ../app/frontnginx frontnginx
-cp -rf ../app/middjango middjango
+pushd .
+
+WORKING_DIR=/tmp/web-start-build
+
+mkdir -p $WORKING_DIR/{nginx_dev,django_dev}
+cp nginx_dev.docker $WORKING_DIR/Dockerfile
+cp django_dev.docker $WORKING_DIR/Dockerfile
+
+cp -rf ../app/nginx_dev $WORKING_DIR/nginx_dev
+cp -rf ../app/django_dev $WORKING_DIR/django_dev
+
+cp docker-compose.yml $WORKING_DIR
+
+cd $WORKING_DIR
+
 docker-compose build
 echo Cleaning up resource files...
-rm -rf frontnginx/frontnginx
-rm -rf middjango/middjango
+rm -rf $WORKING_DIR
+
+popd
